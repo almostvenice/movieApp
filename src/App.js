9774966bd36f04e358ae10ac75d981db2c7fe3ca
David Preciado <almostvenice@gmail.com>
2022-06-05 12:47:50 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from 'react';
+import Movies from './components/movies';
+
+export default class App extends Component {
+render() {    
+    return (
+      <main className='container'>
+        <Movies/>
+      </main>
+    );
+  }
+}
