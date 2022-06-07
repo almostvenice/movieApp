fe85462e9a3fc42afe0207e5637d09482f059ace
David Preciado <almostvenice@gmail.com>
2022-06-07 15:14:01 -0700

    broken table

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
-import Like from './common/like';
 import Pagination from './common/pagination';
-import { Paginate } from '../utils/paginate';
+import { paginate } from '../utils/paginate';
 import ListGroup from './common/listGroup';
 import { getGenres } from '../services/fakeGenreService';
+import MoviesTable from './moviesTable';
+import _ from 'lodash';
 
 
-export default class Movies extends Component {
+class Movies extends Component {
     state = {
         movies: [],
-        pageSize: 4,
+        genres: [],
         currentPage: 1,
-        genres: []
+        pageSize: 4,
+        sortColumn: { path: 'title', order: 'asc' }
     };
+    componentDidMount() {
+        const genres = [{ _id: "", name: 'All Genres'}, ...getGenres()]
+        this.setState({ movies: getMovies(), genres})
+    }
 
     handleDelete = movie => {
         const movies = this.state.movies.filter(m => m._id !== movie._id); //get all the movies except the movie with the ID that was passed
         this.setState({movies: movies})                 // if they key and value are the same you can pass ({ movies })
     };
 
-    componentDidMount() {
-        const genres = [{ name: 'All Genres'}, ...getGenres()]
-        this.setState({ movies: getMovies(), genres: genres})
-    }
 
     handleLike = (movie) => {
         const movies = [...this.state.movies];          //movies is now a copy of the movies state object
@@ -40,74 +42,63 @@ export default class Movies extends Component {
 
     handleGenreSelect = genre => {
         this.setState({selectedGenre: genre, currentPage: 1}) //needs current page for when you are on page 2+ and switch to a genre with only 1 page
+    };
+
+    handleSort = sortColumn => {
+        this.setState({ sortColumn })
     }
 
     render() {
-        const {length: count} = this.state.movies;      //destructuring to get the count of movies.length
-        const {pageSize, currentPage, movies: allMovies, genres, selectedGenre} = this.state;     //destructuring to get the state props
+        const {length: count} = this.state.movies;
+        console.log(count)      //destructuring to get the count of movies.length
+        const {
+            pageSize, 
+            currentPage, 
+            selectedGenre,
+            movies: allMovies,
+            sortColumn 
+        } = this.state;     //destructuring to get the state props
 
         if (count === 0) return <p>There are no movies in the database</p>  //if no movies are in the database (movies.length === 0)
         
         //if selectedGenre is truthy, filter allMovies and return any movie with the identical genere ID. else return all movies if there is no selectedGenre
-        const filtered = selectedGenre && selectedGenre._id ? allMovies.filter(m => m.genre._id === selectedGenre._id) : allMovies;
-            
-        const movies = Paginate(filtered, currentPage, pageSize)
+        const filtered = 
+        selectedGenre && selectedGenre._id 
+            ? allMovies.filter(m => m.genre._id === selectedGenre._id) 
+            : allMovies;
+
+        const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
+
+        const movies = paginate(sorted, currentPage, pageSize);
         
         return (
-            <React.Fragment>
-                <div className="row">
-                    <div className="col-3">
-                        <ListGroup                              //ListGroup props
-                            items={genres} 
-                            onItemSelect={this.handleGenreSelect} // runs first
-                            selectedItem={selectedGenre}          //runs after onItemSelect. now the selectedItem is the current clicked Genere
-                        />
-                    </div>
-                    <div className="col">
-                        <p>Showing {filtered.length} movies in the database</p>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>Title</th>
-                                    <th>Genre</th>
-                                    <th>Stock</th>
-                                    <th>Rate</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {movies.map(movie => (   //map thru the movies array. for each movie create this HTML <tr>...</tr> with a key of {movie._id} from fakeMovieService.js
-                                <tr key={movie._id}>                
-                                    <td>{movie.title}</td>
-                                    <td>{movie.genre.name}</td>
-                                    <td>{movie.numberInStock}</td>
-                                    <td>{movie.dailyRentalRate}</td>
-                                    <td>
-                                        <Like 
-                                            liked={movie.liked} 
-                                            onClick={() => this.handleLike(movie)}
-                                        /> 
-                                    </td>
-                                    <td>
-                                        <button 
-                                            onClick={() => this.handleDelete(movie)} 
-                                            className="btn btn-danger btn-sm">Delete
-                                        </button>
-                                    </td>
-                                </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                        <Pagination 
-                            itemsCount={filtered.length}   //instead of count because now each filtered movies object might have only 1 page of movies                                       //Pagination props
-                            pageSize={pageSize} 
-                            currentPage={currentPage}
-                            onPageChange={this.handlePageChange}
-                        />
-                    </div>
+            <div className="row">
+                <div className="col-3">
+                    <ListGroup                              //ListGroup props
+                        items={this.state.genres} 
+                        onItemSelect={this.handleGenreSelect} // runs first
+                        selectedItem={selectedGenre}          //runs after onItemSelect. now the selectedItem is the current clicked Genere
+                    />
                 </div>
-            </React.Fragment>
+                <div className="col">
+                    <p>Showing {filtered.length} movies in the database</p>
+                    <MoviesTable 
+                        movies={movies} 
+                        sortColumn={sortColumn}
+                        onLike={this.handleLike} 
+                        onDelete={this.handleDelete}
+                        onSort={this.handleSort}
+                    />
+                    <Pagination 
+                        itemsCount={filtered.length}   //instead of count because now each filtered movies object might have only 1 page of movies                                       //Pagination props
+                        pageSize={pageSize} 
+                        currentPage={currentPage}
+                        onPageChange={this.handlePageChange}
+                    />
+                </div>
+            </div>
         )
     };
-
 }
+
+export default Movies;
